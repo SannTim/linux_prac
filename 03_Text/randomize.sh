cbfa0381fcfba8cd55e64f664677e6fd23798e12
SannTim <timohaj1@yandex.ru>
2025-10-02 10:19:59 +0300

    ereased testing lines

--- 03_Text/randomize.sh
+++ 03_Text/randomize.sh
@@ -20,26 +20,13 @@ while IFS= read -r line || [[ -n "$line" ]]; do
 	j=$((j+1))
 	printf "\n"
 done
-# echo $max
-# sleep 10
-# printf "\n\n"
-# echo ${all_sym[@]}
-#
-# echo ${all_sym[4]}
 shuffled_arr=()
 while IFS= read -r -d '' item; do
     shuffled_arr+=("$item")
 done < <(shuf -z -e "${all_sym[@]}")
-# echo Исходный массив:
-# printf "Элемент: %s\n" "${all_sym[@]}"
-# echo Перемешанный:
-# printf "Элемент: %s\n" "${shuffled_arr[@]}"
 for i in "${shuffled_arr[@]}"; do
 	args=($i)
-	# printf "base: %d %d %d %s\n" $time_wait $i
-	# printf "place: %d %d\n" ${args[0]} ${args[1]}
-	# printf "char:%s\n" ${args[2]}
-	tput cup ${args[0]} ${args[1]}
+		tput cup ${args[0]} ${args[1]}
 	printf "%s" ${args[2]}
 	sleep $time_wait
 done
